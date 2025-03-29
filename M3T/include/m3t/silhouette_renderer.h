f2e0b2f132d7a64171ec3cf78c4e31c1c67f0dfc
EMS90 <108662825+EMS90@users.noreply.github.com>
2025-03-29 12:06:54 +0100

    add empty constructors to silhouette renderers to solve sissue on destructor call #2

--- M3T/include/m3t/silhouette_renderer.h
+++ M3T/include/m3t/silhouette_renderer.h
@@ -100,6 +100,7 @@ class FullSilhouetteRenderer : public FullDepthRenderer {
       const std::string &name, const std::filesystem::path &metafile_path,
       const std::shared_ptr<RendererGeometry> &renderer_geometry_ptr,
       const std::shared_ptr<Camera> &camera_ptr);
+  ~FullSilhouetteRenderer();
   bool SetUp() override;
 
   // Setters
@@ -162,6 +163,7 @@ class FocusedSilhouetteRenderer : public FocusedDepthRenderer {
       const std::string &name, const std::filesystem::path &metafile_path,
       const std::shared_ptr<RendererGeometry> &renderer_geometry_ptr,
       const std::shared_ptr<Camera> &camera_ptr);
+  ~FocusedSilhouetteRenderer();
   bool SetUp() override;
 
   // Setters
